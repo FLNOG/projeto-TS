759ede33fcf87f80c5665a015859d8077c22b6e6
FLNOG <felipe@tecnbr.com.br>
2025-10-14 10:10:22 -0300

    chore: organiza inicio do projeto de Testes de Software (Spring Boot Java 21)

---
+++ src/main/java/com/testesoftware/cadastro/CadastroUsuariosProdutosApplication.java
@@ -0,0 +1,13 @@
+package com.testesoftware.cadastro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CadastroUsuariosProdutosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CadastroUsuariosProdutosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/testesoftware/cadastro/CadastroUsuariosProdutosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.testesoftware.cadastro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CadastroUsuariosProdutosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
