f17c55095a7160c36bac89b27c5d2683d83661aa
FLNOG <felipe@tecnbr.com.br>
2025-10-22 13:40:20 -0300

    test(pessoa): adiciona mais testes unitarios para o model Pessoa

--- src/test/java/com/testesoftware/cadastro/model/PessoaTest.java
+++ src/test/java/com/testesoftware/cadastro/model/PessoaTest.java
@@ -25,4 +25,44 @@ public class PessoaTest {
         assertEquals("felipe@email.com",  pessoa.getEmail());
         assertEquals("1234567890", pessoa.getSenha());
     }
+
+    @Test
+    public void testNewPessoaWithCep(){
+        Pessoa pessoa = new Pessoa("Felipe Nogueira", "felipe@email.com", "1234567890", "01001000");
+        pessoa.setLogradouro("Praça da Sé");
+        pessoa.setBairro("Sé");
+        pessoa.setLocalidade("São Paulo");
+        pessoa.setUf("SP");
+
+        assertEquals("Felipe Nogueira", pessoa.getNome());
+        assertEquals("felipe@email.com", pessoa.getEmail());
+        assertEquals("1234567890", pessoa.getSenha());
+        assertEquals("01001000", pessoa.getCep());
+        assertEquals("Praça da Sé", pessoa.getLogradouro());
+        assertEquals("Sé", pessoa.getBairro());
+        assertEquals("São Paulo", pessoa.getLocalidade());
+        assertEquals("SP", pessoa.getUf());
+    }
+
+    @Test
+    public void testGetSetAllFields(){
+        Pessoa pessoa = new Pessoa();
+        pessoa.setNome("Felipe Nogueira");
+        pessoa.setEmail("felipe@email.com");
+        pessoa.setSenha("1234567890");
+        pessoa.setCep("01001000");
+        pessoa.setLogradouro("Praça da Sé");
+        pessoa.setBairro("Sé");
+        pessoa.setLocalidade("São Paulo");
+        pessoa.setUf("SP");
+
+        assertEquals("Felipe Nogueira", pessoa.getNome());
+        assertEquals("felipe@email.com", pessoa.getEmail());
+        assertEquals("1234567890", pessoa.getSenha());
+        assertEquals("01001000", pessoa.getCep());
+        assertEquals("Praça da Sé", pessoa.getLogradouro());
+        assertEquals("Sé", pessoa.getBairro());
+        assertEquals("São Paulo", pessoa.getLocalidade());
+        assertEquals("SP", pessoa.getUf());
+    }
 }
