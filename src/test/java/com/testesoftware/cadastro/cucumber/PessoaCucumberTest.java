a9c836c97abe9c70c7a2743a23c442d2b20106b9
Yacsu77 <contatailandia134@gmail.com>
2025-11-09 01:32:43 -0300

    test: Adicinado Configuracao para BDD

---
+++ src/test/java/com/testesoftware/cadastro/cucumber/PessoaCucumberTest.java
@@ -0,0 +1,25 @@
+package com.testesoftware.cadastro.cucumber;
+
+import static io.cucumber.junit.platform.engine.Constants.GLUE_PROPERTY_NAME;
+import static io.cucumber.junit.platform.engine.Constants.PLUGIN_PROPERTY_NAME;
+
+import org.junit.platform.suite.api.ConfigurationParameter;
+import org.junit.platform.suite.api.IncludeEngines;
+import org.junit.platform.suite.api.SelectClasspathResource;
+import org.junit.platform.suite.api.Suite;
+
+@Suite
+@IncludeEngines("cucumber")
+@SelectClasspathResource("features/pessoa.feature")
+@ConfigurationParameter(
+    key = GLUE_PROPERTY_NAME,
+    value = "com.testesoftware.cadastro.stepdefinitions"
+)
+@ConfigurationParameter(
+    key = PLUGIN_PROPERTY_NAME,
+    value = "pretty, html:target/cucumber-reports/index.html, json:target/cucumber-reports/cucumber.json"
+)
+public class PessoaCucumberTest {
+}
+
+
